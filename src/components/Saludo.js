9ed5dba7f00552303f0914b91378ece15d11ee47
Jesus <esparza100401@gmail.com>
2024-11-13 21:56:06 -0600

    comentario

--- src/components/Saludo.js
+++ src/components/Saludo.js
@@ -6,3 +6,15 @@ function Saludo({ nombre }) {
 }
 
 export default Saludo;
+
+/*import React from 'react';
+
+function Saludo(props) {
+  return <h1>Hola, {props.nombre}!</h1>;
+}
+
+export default Saludo;
+La mayoría de los proyectos modernos permiten usar la extensión .js
+incluso si hay JSX en el archivo, ya que las herramientas
+de construcción como Babel pueden manejar la conversión del JSX
+automáticamente.*/
